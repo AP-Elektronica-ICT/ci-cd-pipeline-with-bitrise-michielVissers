c3d727ad933a6843dff6bdbba9bda2d67b3a14b1
MichielVissers <michiel.vissers09@gmail.com>
2018-12-06 13:18:12 +0100

    first commit

---
+++ Bitrise/Bitrise project1/app/src/main/java/com/example/michi/mytestapplication/Utils.java
@@ -0,0 +1,9 @@
+package com.example.michi.mytestapplication;
+
+public class Utils {
+
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+}
---
+++ Bitrise/Bitrise project1/app/src/test/java/com/example/michi/mytestapplication/ExampleUnitTest.java
@@ -0,0 +1,25 @@
+package com.example.michi.mytestapplication;
+
+import com.example.michi.mytestapplication.Utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void add_isCorrect() throws Exception {
+        assertEquals(4, Utils.addNumbers(2 , 2));
+    }
+
+    @Test
+    public void add_twodigits_isCorrect() throws Exception {
+        assertEquals(44, Utils.addNumbers(22 , 22));
+    }
+
+    @Test
+    public void add_big_isCorrect() throws Exception {
+        assertEquals(4444, Utils.addNumbers(2222 , 2222));
+    }
+
+}
